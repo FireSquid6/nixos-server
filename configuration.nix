46311140e482d11b24604d0bf8c8efa478bf973b
firesquid6 <jdeiss06@gmail.com>
2024-01-28 17:41:03 -0600

    add new config

--- configuration.nix
+++ configuration.nix
@@ -1,64 +1,75 @@
-{ config, pkgs, lib, ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
 
-let
-  hostname = "pi0";
-  user = "firesquid";
-  password = "8008";
-  nixosHardwareVersion = "7f1836531b126cfcf584e7d7d71bf8758bb58969";
+{ config, lib, pkgs, ... }:
 
-  timeZone = "America/New_York";
-  defaultLocale = "en_US.UTF-8";
-in {
-  imports = ["${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/${nixosHardwareVersion}.tar.gz" }/raspberry-pi/4"];
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = [ "noatime" ];
+  environment.pathsToLink = [ "/libexec" ];
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+  };
+
+  virtualisation.docker.enable = true;
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+
+  services.tailscale.enable = true;
+
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+   
+    displayManager = {
+        defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+     ];
     };
   };
 
-  networking.hostName = hostname;
+  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    gh
-    neovim
-    git
-    vim
-  ];
+  networking.hostName = "pi0"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  services.openssh.enable = true;
+  hardware.opengl.enable = true;
 
-  time.timeZone = timeZone;
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
 
-  i18n = {
-    defaultLocale = defaultLocale;
-    extraLocaleSettings = {
-      LC_ADDRESS = defaultLocale;
-      LC_IDENTIFICATION = defaultLocale;
-      LC_MEASUREMENT = defaultLocale;
-      LC_MONETARY = defaultLocale;
-      LC_NAME = defaultLocale;
-      LC_NUMERIC = defaultLocale;
-      LC_PAPER = defaultLocale;
-      LC_TELEPHONE = defaultLocale;
-      LC_TIME = defaultLocale;
-    };
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+
+  users.users.firesquid = {
+    isNormalUser = true;
+    password = "8008";
+    extraGroups = [ "wheel" "networkmanager" "docker"];
   };
 
-  users = {
-    mutableUsers = false;
-    users."${user}" = {
-      isNormalUser = true;
-      password = password;
-      extraGroups = [ "wheel" ];
-    };
-  };
-
-  # Enable passwordless sudo.
   security.sudo.extraRules= [
-    {  users = [ user ];
+    {  users = [ "firesquid" ];
       commands = [
          { command = "ALL" ;
            options= [ "NOPASSWD" ];
@@ -67,8 +78,43 @@ in {
     }
   ];
 
-  # Enable GPU acceleration
-  hardware.raspberry-pi."4".fkms-3d.enable = true;
+  environment.systemPackages = with pkgs; [
+    gh
+    neovim
+    git
+    lazygit
+    vim
+    kitty
+    tmux
+    htop
+    direnv
+  ];
 
-  system.stateVersion = "23.11";
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
+
